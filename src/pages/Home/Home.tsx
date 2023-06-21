705871ca36a17cee46208451b5b54afb96755a58
Anthony.Albacete <Anthony.Albacete@rjet.com>
2023-06-21 18:18:32 -0400

    initial commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,20 @@
-import { Box, Title, Image, Stack, RingProgress, Text, Badge, Flex } from "@mantine/core";
+import {
+  Box,
+  Title,
+  Image,
+  Stack,
+  RingProgress,
+  Text,
+  Badge,
+  Flex,
+} from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
 import React from "react";
-import { useIndividualShowDetails, usePopularMovies, useUpcomingMovies } from "../../data";
+import {
+  useIndividualShowDetails,
+  usePopularMovies,
+  useUpcomingMovies,
+} from "../../data";
 
 export type Movie = {
   id: number;
@@ -15,7 +28,7 @@ export type Movie = {
 type Genre = {
   id: number;
   name: string;
-}
+};
 
 type TVShow = {
   id?: number;
@@ -23,30 +36,27 @@ type TVShow = {
   backdropPath?: string;
   overview?: string;
   genres?: Genre[];
-}
+};
 
 const getRatingColor = (average: number) => {
   if (average > 7.5) return "green";
   if (average > 5.0) return "yellow"; //
   return "red";
-}
+};
 
 export const Home: React.FC = () => {
-  const popularMoviesQuery = usePopularMovies(
-    {
-      language: "en",
-      page: "1",
-    },
-    { suspense: true }
-  );
+  const popularMoviesQuery = usePopularMovies({
+    language: "en",
+    page: "1",
+  });
 
   const upcomingMoviesQuery = useUpcomingMovies({
     language: "en-US",
     page: "1",
-    region: "US"
+    region: "US",
   });
 
-  const tvShowQuery = useIndividualShowDetails({ seriesId: 125988 })
+  const tvShowQuery = useIndividualShowDetails({ seriesId: 125988 });
 
   const popularMovies: Movie[] = popularMoviesQuery.data?.results;
   const upcomingMovies: Movie[] = upcomingMoviesQuery.data?.results;
@@ -56,19 +66,47 @@ export const Home: React.FC = () => {
     <Box sx={{ color: "white" }}>
       <Stack spacing={"xl"} sx={{ padding: "1.5rem" }}>
         <Box>
-          <Title sx={{ paddingBottom: "0.5rem" }} order={5}>CURRENTLY TRENDING</Title>
-          <Carousel slideSize={"20rem"} slideGap={"lg"} align="start" slidesToScroll={"auto"} dragFree>
-            {popularMovies.map(({ id, poster_path, vote_average }) => {
+          <Title sx={{ paddingBottom: "0.5rem" }} order={5}>
+            CURRENTLY TRENDING
+          </Title>
+          <Carousel
+            slideSize={"20rem"}
+            slideGap={"lg"}
+            align="start"
+            slidesToScroll={"auto"}
+            dragFree
+          >
+            {popularMovies.map(({ id, title, poster_path, vote_average }) => {
               return (
                 <Carousel.Slide key={id}>
-                  <Image src={`https://image.tmdb.org/t/p/original/${poster_path}`} onClick={(e) => e.stopPropagation()} />
+                  <Image
+                    src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+                    onClick={(e) => e.stopPropagation()}
+                    withPlaceholder
+                    placeholder={
+                      <Image
+                        sx={{ height: "20rem" }}
+                        src="./popcorn.svg"
+                      ></Image>
+                    }
+                  />
                   <RingProgress
                     sx={{ top: -40 }}
                     size={70}
                     thickness={8}
-                    sections={[{ value: vote_average * 10, color: getRatingColor(vote_average) }]}
+                    sections={[
+                      {
+                        value: vote_average * 10,
+                        color: getRatingColor(vote_average),
+                      },
+                    ]}
                     label={
-                      <Text color={getRatingColor(vote_average)} weight={700} align="center" size="xs">
+                      <Text
+                        color={getRatingColor(vote_average)}
+                        weight={700}
+                        align="center"
+                        size="xs"
+                      >
                         {vote_average * 10}%
                       </Text>
                     }
@@ -79,26 +117,57 @@ export const Home: React.FC = () => {
           </Carousel>
         </Box>
         <Box>
-          <Title sx={{ paddingBottom: "0.5rem" }} order={5}>UPCOMING TITLES</Title>
-          <Carousel slideSize={"10rem"} slideGap={"md"} align="start" slidesToScroll={"auto"} dragFree withControls={false}>
-            {upcomingMovies.map(({ id, poster_path }) => {
+          <Title sx={{ paddingBottom: "0.5rem" }} order={5}>
+            UPCOMING TITLES
+          </Title>
+          <Carousel
+            slideSize={"10rem"}
+            slideGap={"md"}
+            align="start"
+            slidesToScroll={"auto"}
+            dragFree
+            withControls={false}
+          >
+            {upcomingMovies.map(({ id, title, poster_path }) => {
               return (
                 <Carousel.Slide key={id}>
-                  <Image src={`https://image.tmdb.org/t/p/original/${poster_path}`} onClick={(e) => e.stopPropagation()} />
+                  <Image
+                    src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+                    onClick={(e) => e.stopPropagation()}
+                    withPlaceholder
+                    placeholder={
+                      <Text sx={{ height: "10rem" }} align="center">
+                        {title}
+                      </Text>
+                    }
+                  />
                 </Carousel.Slide>
               );
             })}
           </Carousel>
         </Box>
         <Flex direction="column" gap="sm">
+          <Title sx={{}} order={5}>
+            NEW IN TV
+          </Title>
           <Box sx={{ position: "relative" }}>
-            <Image src={`https://image.tmdb.org/t/p/original/${tvShow.backdropPath}`} height="40rem" onClick={(e) => e.stopPropagation()} />
-            <Title sx={{ position: "absolute", bottom: 0, left: 20 }}>{tvShow.name}</Title>
+            <Image
+              src={`https://image.tmdb.org/t/p/original/${tvShow.backdropPath}`}
+              height="40rem"
+              onClick={(e) => e.stopPropagation()}
+            />
+            <Title sx={{ position: "absolute", bottom: 0, left: 15 }}>
+              {tvShow.name}
+            </Title>
           </Box>
           <Text fz="md">{tvShow.overview}</Text>
           <Flex gap="md">
-            {tvShow.genres?.map((genre) => {
-              return <Badge color="gray" radius="xs" size="xl">{genre.name}</Badge>
+            {tvShow.genres?.map(({ id, name }) => {
+              return (
+                <Badge key={id} color="gray" radius="xs" size="xl">
+                  {name}
+                </Badge>
+              );
             })}
           </Flex>
         </Flex>
