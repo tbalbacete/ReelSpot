47fee306c74113733b232299f95d8b89a78ea948
Anthony.Albacete <Anthony.Albacete@rjet.com>
2023-06-22 10:01:16 -0400

    initial commit

--- src/data/hooks/useApi.ts
+++ src/data/hooks/useApi.ts
@@ -3,7 +3,7 @@ import { BaseAPI, Configuration } from "@/api";
 type ApiConstructor<T extends BaseAPI> = new (config: Configuration) => T;
 
 export function useApi<T extends BaseAPI>(api: ApiConstructor<T>) {
-  const apiKey = window.env?.REACT_APP_API_KEY;
+  const apiKey = import.meta.env?.REACT_APP_API_KEY;
   return new api(
     new Configuration({
       basePath: "https://api.themoviedb.org",
