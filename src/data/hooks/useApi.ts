ed08ec61e50bc2c1bf921f308397d17829942098
Anthony.Albacete <Anthony.Albacete@rjet.com>
2023-06-22 18:19:25 -0400

    change api key

--- src/data/hooks/useApi.ts
+++ src/data/hooks/useApi.ts
@@ -3,12 +3,12 @@ import { BaseAPI, Configuration } from "@/api";
 type ApiConstructor<T extends BaseAPI> = new (config: Configuration) => T;
 
 export function useApi<T extends BaseAPI>(api: ApiConstructor<T>) {
-  const apiKey = import.meta.env?.REACT_APP_API_KEY;
+  const apiKey = process.env?.REACT_APP_API_KEY;
   return new api(
     new Configuration({
       basePath: "https://api.themoviedb.org",
       headers: {
-        ["Authorization"]: `Bearer ${window.env?.REACT_APP_API_KEY}`,
+        ["Authorization"]: `Bearer ${apiKey}`,
       },
       middleware: [],
     })
