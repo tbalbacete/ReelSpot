db32ea309d7d21e8b69ec709154b0b6e43cb8557
Tony Albacete <anthony.albacete@rjet.com>
2024-01-08 11:35:32 -0500

    initial commit

--- src/data/context.tsx
+++ src/data/context.tsx
@@ -1,17 +1,6 @@
 import * as React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-      suspense: true,
-      refetchOnWindowFocus: false,
-      useErrorBoundary: true,
-      retry: false,
-    },
-  },
-});
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/queryClient";
 
 type Props = {
   children?: React.ReactNode;
